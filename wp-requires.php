3302cf617ad7cf108c35232407dd53f30da52c7a
szepe.viktor <szepe.viktor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-23 18:47:29 +0000

    added tag 1.5
    git-svn-id: https://plugins.svn.wordpress.org/whats-running/tags/1.5@937329 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-requires.php
+++ wp-requires.php
@@ -3,7 +3,7 @@
  * Plugin Name: What's running
  * Plugin URI: http://wordpress.org/plugins/whats-running/
  * Description: Lists WordPress require() calls mainly for plugin code refactoring
- * Version: 1.4
+ * Version: 1.5
  * Author: Viktor Szépe
  * Author URI: http://www.online1.hu/webdesign/
  * License: GNU General Public License (GPL) version 2
@@ -12,7 +12,7 @@
 /*  Copyright 2014  Viktor Szépe  (email: viktor@szepe.net)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -25,42 +25,42 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-if (false === defined('ABSPATH')) {
+if ( false === defined( 'ABSPATH' ) ) {
     die();
 }
 
 
 function whats_running() {
     // on file uploads (async-upload.php) DOING_AJAX is defined late
-    if ((defined('DOING_AJAX') && DOING_AJAX) ||
-        (defined('DOING_CRON') && DOING_CRON) ||
-        (@$_SERVER['SCRIPT_FILENAME'] === ABSPATH . 'wp-admin/async-upload.php')) {
+    if ( ( defined('DOING_AJAX') && DOING_AJAX ) ||
+        ( defined('DOING_CRON') && DOING_CRON ) ||
+        ( @$_SERVER['SCRIPT_FILENAME'] === ABSPATH . 'wp-admin/async-upload.php' ) ) {
         return;
     }
     // run on IFRAME_REQUEST
 
     $abslen = strlen(ABSPATH);
 
-    echo '<br style="clear:both;"/><hr/><br/><pre style="padding-left:160px;"><ol style="list-style-position:inside;">';
-    foreach (get_included_files() as $i => $path) {
+    echo '<br style="clear:both;"/><hr/><br/><pre style="padding-left:160px;font:14px/140% monospace;background:#FFF;"><ol style="list-style-position:inside;">';
+    foreach ( get_included_files() as $i => $path ) {
         $color = ' style="color:red;"';
-        if (0 === strpos($path, WP_PLUGIN_DIR)) {
+        if ( 0 === strpos( $path, WP_PLUGIN_DIR ) ) {
             $color = ' style="color:blue;"';
-        } elseif (0 === strpos($path, WP_CONTENT_DIR . '/themes')) {
+        } elseif ( 0 === strpos($path, WP_CONTENT_DIR . '/themes' ) ) {
             $color = ' style="color:orange;"';
         }
         // only after WP_CONTENT_DIR check
-        if (0 === strpos($path, ABSPATH)) {
-            $path = substr($path, $abslen);
+        if ( 0 === strpos($path, ABSPATH ) ) {
+            $path = substr( $path, $abslen );
         }
-        if (0 === strpos($path, WPINC)) {
+        if ( 0 === strpos( $path, WPINC ) ) {
             $color = ' style="color:green;"';
-        } elseif (0 === strpos($path, 'wp-admin')) {
+        } elseif ( 0 === strpos($path, 'wp-admin' ) ) {
             $color = ' style="color:grey;"';
         }
-        printf('<li%s>%s</li>', $color, esc_html($path));
+        printf( '<li%s>%s</li>', $color, esc_html( $path ) );
     }
     echo '</ol></pre>';
 }
 
-add_action('shutdown', 'whats_running');
+add_action( 'shutdown', 'whats_running' );
