9b32236625eb107089bc20938e5784d6c902f47d
szepe.viktor <szepe.viktor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-12 14:10:35 +0000

    1.8: added higlight
    git-svn-id: https://plugins.svn.wordpress.org/whats-running/tags/1.8@988302 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: szepeviktor
 Donate link: https://szepe.net/wp-donate/
 Tags: debug, debugging, developer, development, performance, profiler, profiling
 Requires at least: 3.5
-Tested up to: 3.9
-Stable tag: 1.3
+Tested up to: 4.0
+Stable tag: 1.8
 License: GPLv2
 
 Lists WordPress require() calls mainly for plugin code refactoring
@@ -13,19 +13,32 @@ Lists WordPress require() calls mainly for plugin code refactoring
 
 = Only for development! =
 
-This plugin dumps the colorized filenames after the normal WordPress output, after the closing html tag.
-This generates invalid HTML but gives you an overview of plugins and the current theme.
+This plugin dumps the colorized filenames and filesizes after the normal WordPress output, after the closing html tag.
+This generates invalid HTML but gives you an overview of loaded plugins and the current theme.
 
-It lists all files parsed and executed by the PHP engine. It can be used for plugin or theme refactoring.
+*What's running* lists all files parsed and executed by the PHP engine.
+It can be used for plugin or theme refactoring.
 
-* files in the plugin directory are BLUE
-* files in the themes directory are ORANGE
-* files in the wp-includes directory are GREEN
-* files in the wp-admin directory are GREY
-* all other files are RED
+= Now you can highlight your own plugin and theme =
+
+Provide a part of its path:
+
+`define( 'WHATS_RUNNING_HIGHLIGHT', 'wp-content/plugins/my-plugin' );`
+
+* your code is highlighted with YELLOW background
+* plugin files are in BLUE
+* themes files are in ORANGE
+* files in the wp-includes directory are in GREEN
+* files in the wp-admin directory are in GREY
+* all other files are in RED
+* the PINK bar after the filename represents the file size ( 0.5 kB/px )
+
+You can watch [this WP core bug](https://core.trac.wordpress.org/ticket/28364) to get more information about WordPress entry points.
 
 You can find some documentaion here what makes a WordPress plugin efficient.
 https://github.com/szepeviktor/WPHW
+
+[GitHub](https://github.com/szepeviktor/whats-running)
 
 == Installation ==
 
@@ -42,9 +55,24 @@ After finishing the refactor, please deactivate and delete this plugin.
 
 == Screenshots ==
 
-1. This is a screen shot of the dashboard. You can see the filenames after the admin footer.
+1. This is an old screen shot. You can see the filenames after the admin footer.
+2. This is the current screen shot of the plugin's output on the dashboard. The colored bars represent the file sizes.
 
 == Changelog ==
+
+= 1.8 =
+* Changed wrapper element to div with id "whats-running"
+* Added feature to highlight your code.
+
+= 1.7 =
+* Added size in kB to the title of the file size lines
+
+= 1.6 =
+* Added file sizes and total file size
+
+= 1.5 =
+* Added inline styles
+* Link to WP core bug to watch
 
 = 1.4 =
 * FIX: don't run on non-AJAX media uploads (async-upload.php)
--- wp-requires.php
+++ wp-requires.php
@@ -3,7 +3,7 @@
  * Plugin Name: What's running
  * Plugin URI: http://wordpress.org/plugins/whats-running/
  * Description: Lists WordPress require() calls mainly for plugin code refactoring
- * Version: 1.4
+ * Version: 1.8
  * Author: Viktor Szépe
  * Author URI: http://www.online1.hu/webdesign/
  * License: GNU General Public License (GPL) version 2
@@ -12,7 +12,7 @@
 /*  Copyright 2014  Viktor Szépe  (email: viktor@szepe.net)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -25,42 +25,57 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-if (false === defined('ABSPATH')) {
+if ( false === defined( 'ABSPATH' ) ) {
     die();
 }
 
 
 function whats_running() {
     // on file uploads (async-upload.php) DOING_AJAX is defined late
-    if ((defined('DOING_AJAX') && DOING_AJAX) ||
-        (defined('DOING_CRON') && DOING_CRON) ||
-        (@$_SERVER['SCRIPT_FILENAME'] === ABSPATH . 'wp-admin/async-upload.php')) {
+    if ( ( defined('DOING_AJAX') && DOING_AJAX ) ||
+        ( defined('DOING_CRON') && DOING_CRON ) ||
+        ( @$_SERVER['SCRIPT_FILENAME'] === ABSPATH . 'wp-admin/async-upload.php' ) ) {
         return;
     }
-    // run on IFRAME_REQUEST
+    // do run on IFRAME_REQUEST
 
     $abslen = strlen(ABSPATH);
+    $total_size = 0;
+    $highlight = defined( 'WHATS_RUNNING_HIGHLIGHT' ) ? WHATS_RUNNING_HIGHLIGHT : false;
 
-    echo '<br style="clear:both;"/><hr/><br/><pre style="padding-left:160px;"><ol style="list-style-position:inside;">';
-    foreach (get_included_files() as $i => $path) {
-        $color = ' style="color:red;"';
-        if (0 === strpos($path, WP_PLUGIN_DIR)) {
-            $color = ' style="color:blue;"';
-        } elseif (0 === strpos($path, WP_CONTENT_DIR . '/themes')) {
-            $color = ' style="color:orange;"';
+    print '<div id="whats-running" style="clear:both;"/><hr/><pre style="padding-left:160px;font:14px/140% monospace;background:#FFF;"><ol style="list-style-position:inside;">';
+    foreach ( get_included_files() as $i => $path ) {
+        $size = filesize( $path );
+        $total_size += $size;
+        $color = 'red';
+
+        if ( $highlight && false !== strpos( $path, $highlight ) ) {
+            $background = 'background:yellow;';
+        } else {
+            $background = '';
+        }
+
+        if ( 0 === strpos( $path, WP_PLUGIN_DIR ) ) {
+            $color = 'blue';
+        } elseif ( 0 === strpos($path, WP_CONTENT_DIR . '/themes' ) ) {
+            $color = 'orange';
         }
         // only after WP_CONTENT_DIR check
-        if (0 === strpos($path, ABSPATH)) {
-            $path = substr($path, $abslen);
+        if ( 0 === strpos($path, ABSPATH ) ) {
+            $path = substr( $path, $abslen );
         }
-        if (0 === strpos($path, WPINC)) {
-            $color = ' style="color:green;"';
-        } elseif (0 === strpos($path, 'wp-admin')) {
-            $color = ' style="color:grey;"';
+        if ( 0 === strpos( $path, WPINC ) ) {
+            $color = 'green';
+        } elseif ( 0 === strpos($path, 'wp-admin' ) ) {
+            $color = 'grey';
         }
-        printf('<li%s>%s</li>', $color, esc_html($path));
+
+        printf( '<li style="color:%s;%s">%s<span title="%s kB" style="padding-left:%spx;display:inline-block;background-color:#FF00FF;border-radius:5px;height:5px;margin-left:5px;"></span></li>',
+            $color, $background, esc_html( $path ), number_format( $size / 1024, 0 ), round( $size / 512 + 1 ) );
     }
-    echo '</ol></pre>';
+    printf( '<li style="color:black;font-weight:bold;list-style:none;">Total: %s bytes</li>',
+        number_format( $total_size, 0, '.', ' ' ) );
+    print '</ol></pre></div>';
 }
 
-add_action('shutdown', 'whats_running');
+add_action( 'shutdown', 'whats_running' );
